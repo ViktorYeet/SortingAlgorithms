dfa8f17a305da1561fd239ffd9f7b16ee27843fd
ViktorYeet <viktor@stefansbo.se>
2024-09-09 19:42:36 +0200

    MergeSort working optimally i think

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     /**
      * Sort an array by finding the largest element and swapping it with the last unsorted element of the array.
      * Modifies the array in place
-     * @param array
+     * @param array comparable array
      */
     public static <T extends Comparable<T>> void SelectionSort(T[] array) {
         int high = array.length - 1;
@@ -32,7 +32,7 @@ public class Main {
     /**
      * Sort an array by looking at the first two elements put them in order then take the next element and slide it into the correct place.
      * Modifies the array in place
-     * @param array
+     * @param array comparable array
      */
     public static <T extends Comparable<T>> void InsertionSort(T[] array) {
         for (int i = 1; i < array.length; i++) {
@@ -47,7 +47,7 @@ public class Main {
     /**
      * Sort an array by divide and conquer, split the list in half recursively until we have single elements in the segments, feed back into the original array in sorted order.
      * Modifies the array in place
-     * @param array
+     * @param array comparable array
      */
     public static <T extends Comparable<T>> void MergeSort(T[] array) {
         System.arraycopy(MergeSortHelper(array, 0, array.length - 1), 0, array, 0, array.length); // wanted to make it modify and not return
@@ -55,11 +55,11 @@ public class Main {
 
     /**
      * Recursive implementation of merge sort
-     * @param array
-     * @param low
-     * @param high
+     * @param array comparable array
+     * @param low lowerbound to be included in this segment
+     * @param high upperbound to be included in this segment
      * @return
-     * @param <T>
+     * @param <T> Comparable
      */
     private static <T extends Comparable<T>> T[] MergeSortHelper(T[] array, int low, int high) {
         //fracture the array
@@ -71,7 +71,10 @@ public class Main {
             array2 = MergeSortHelper(array, mid + 1, high);
         }
         //if low >= high then the array is either empty or contains 1 element
-        else return (T[])new Comparable[]{array[low]};
+        else if(low == high) {
+            return (T[])new Comparable[]{array[low]};
+        }
+        else return (T[])new Comparable[0];
 
         // Merge the two segments and return the resulting array
         T[] answer = (T[]) new Comparable[high - low + 1];
@@ -98,8 +101,8 @@ public class Main {
         Integer[] bigArray = rand.ints(1000,0, 1000).boxed().toArray(Integer[]::new);
 
 
-        MergeSort(smallArray);
-//        MergeSort(smallArray);
-        System.out.println(Arrays.toString(smallArray));
+        MergeSort(bigArray);
+//        MergeSort(bigArray);
+        System.out.println(Arrays.toString(bigArray));
     }
 }
